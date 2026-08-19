19f2169b146a2c72c1ac8b20c833cb084376239d
Vivek Singh <svivek00singh@gmail.com>
2026-08-19 22:58:26 +0530

    for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,9 @@ const connectDB= require("./src/db/db")
 
 connectDB()
 
-PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT , ()=>{
-    console.log(`server is running on ${PORT}`)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
