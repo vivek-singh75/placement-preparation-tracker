3b561055ebd93309e5f04f3dc76226ee3549cff1
Vivek Singh <svivek00singh@gmail.com>
2026-08-20 09:24:39 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
-const Users= require('./routes/registerUserR');
+const Users = require("./routes/registerUserR");
 const cors = require("cors");
-const app = express()
+
+const app = express();
+
+const allowedOrigins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://vivek-singh75.github.io"
+];
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use('/api/user', Users)
-app.use('/api/user', Users)
+app.use("/api/user", Users);
 
-
-
-module.exports = app
+module.exports = app;
